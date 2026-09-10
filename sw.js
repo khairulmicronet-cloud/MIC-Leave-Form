01290b6f02f166906a3267488c4defdd21c17191
Khairul Github <khairul.micronet@gmail.com>
2026-09-10 14:52:59 +0800

    Bump service worker cache version to v8 and precache leave-import.js
    Updated cache name to version 8 and added leave-import.js to assets.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "leave-form-cache-v7";
+const CACHE_NAME = "leave-form-cache-v8";
 const ASSETS = [
   "./",
   "./index.html",
@@ -8,6 +8,7 @@ const ASSETS = [
   "./manifest.json",
   "./leave-breakdown.html",
   "./leave-breakdown.js",
+  "./leave-import.js",
   "./assets/micronet-logo.png",
   "./assets/leave-form-template.docx",
   "./icons/icon-192.png",
